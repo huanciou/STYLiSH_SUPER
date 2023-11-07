412a45ea2d0af95d317c3ea36dc8bd258546fb4a
Joazen <johnsons544@gmail.com>
2023-11-07 14:58:29 +0800

    fixing order checkout

--- server/dist/controllers/order.js
+++ server/dist/controllers/order.js
@@ -1,39 +1,96 @@
 import axios from "axios";
 import * as dotenv from "dotenv";
 import { ValidationError } from "../utils/errorHandler.js";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import { product, orderModel } from "../schema/schema.js";
 dotenv.config();
 const TAPPAY_PARTNER_KEY = process.env.TAPPAY_PARTNER_KEY;
 const TAPPAY_MERCHANT_ID = process.env.TAPPAY_MERCHANT_ID;
+// async function checkStockAndDecrease(list: listItem[]) {
+//   for (const item of list) {
+//     const productToUpdate = await product.findOne({
+//       _id: item.id,
+//       color: { $in: [item.color.code] },
+//       size: { $in: [item.size] },
+//     });
+//     if (!productToUpdate) {
+//       throw new ValidationError(`Product ${item.id} not found`);
+//     }
+//     const updatedStock = productToUpdate.stock.map((stock, index) => {
+//       if (stock >= item.qty) {
+//         return stock - item.qty;
+//       } else {
+//         throw new ValidationError(`Product ${item.id} stock not enough`);
+//       }
+//     });
+//     productToUpdate.stock = updatedStock;
+//     await productToUpdate.save();
+//   }
+// }
+// async function checkStockAndDecrease(list: listItem[]) {
+//   for (const item of list) {
+//     const query = {
+//       _id: item.id,
+//       color: { $in: [item.color.code] },
+//       size: { $in: [item.size] },
+//       stock: { $gte: item.qty },
+//     };
+//     const update = {
+//       $inc: { stock: -item.qty },
+//     };
+//     const productToUpdate = await product.findOneAndUpdate(query, update, {
+//       new: true,
+//     });
+//     console.log(productToUpdate);
+//     if (!productToUpdate) {
+//       throw new ValidationError(
+//         `Product ${item.id} not found or stock not enough`
+//       );
+//     }
+//   }
+// }
 async function checkStockAndDecrease(list) {
     for (const item of list) {
-        const productToUpdate = await product.findOne({
+        const query = {
             _id: item.id,
-            color: { $in: [item.color.code] },
-            size: { $in: [item.size] },
+            color: item.color.code,
+            size: item.size,
+            stock: { $gte: item.qty },
+        };
+        const update = {
+            $inc: {
+                "stock.$": -item.qty, // 使用 $ 符号来指定匹配的 stock 数组索引
+            },
+        };
+        const productToUpdate = await product.findOneAndUpdate(query, update, {
+            new: true,
         });
+        console.log(productToUpdate);
         if (!productToUpdate) {
-            throw new ValidationError(`Product ${item.id} not found`);
+            throw new ValidationError(`Product ${item.id} not found or stock not enough`);
         }
-        const updatedStock = productToUpdate.stock.map((stock, index) => {
-            if (stock >= item.qty) {
-                return stock - item.qty;
-            }
-            else {
-                throw new ValidationError(`Product ${item.id} stock not enough`);
-            }
-        });
-        productToUpdate.stock = updatedStock;
-        await productToUpdate.save();
     }
 }
 export const checkout = async (req, res) => {
     const userId = res.locals.userId;
     const { prime, order } = req.body;
+    console.log(JSON.stringify(order, null, 4));
     const { shipping, payment, subtotal, freight, total, recipient, list } = order;
+    console.log(shipping);
     const { name, phone, email, address, time } = recipient;
-    const orderDetails = [userId, shipping, payment, subtotal, freight, total, name, phone, email, address, time];
+    const orderDetails = [
+        userId,
+        shipping,
+        payment,
+        subtotal,
+        freight,
+        total,
+        name,
+        phone,
+        email,
+        address,
+        time,
+    ];
     const session = await mongoose.startSession();
     try {
         session.startTransaction();
@@ -41,28 +98,28 @@ export const checkout = async (req, res) => {
         // create order
         const newOrder = await orderModel.create(req.body);
         const orderId = newOrder._id.toString();
-        const response = await axios.post('https://sandbox.tappaysdk.com/tpc/payment/pay-by-prime', {
-            "prime": prime,
-            "partner_key": TAPPAY_PARTNER_KEY,
-            "merchant_id": TAPPAY_MERCHANT_ID,
-            "details": "TapPay Test",
-            "amount": total,
-            "cardholder": {
-                "phone_number": phone,
-                "name": name,
-                "email": email,
-                "address": address,
+        const response = await axios.post("https://sandbox.tappaysdk.com/tpc/payment/pay-by-prime", {
+            prime: prime,
+            partner_key: TAPPAY_PARTNER_KEY,
+            merchant_id: TAPPAY_MERCHANT_ID,
+            details: "TapPay Test",
+            amount: total,
+            cardholder: {
+                phone_number: phone,
+                name: name,
+                email: email,
+                address: address,
             },
-            "remember": false
+            remember: false,
         }, {
             headers: {
                 "Content-Type": "application/json",
-                "x-api-key": TAPPAY_PARTNER_KEY
-            }
+                "x-api-key": TAPPAY_PARTNER_KEY,
+            },
         });
         console.log(response.data);
         if (response.data.status !== 0) {
-            return res.status(400).json({ message: 'Transaction Failed' });
+            return res.status(400).json({ message: "Transaction Failed" });
         }
         // // create order
         // const newOrder = await orderModel.create(req.body)
@@ -70,8 +127,8 @@ export const checkout = async (req, res) => {
         await session.commitTransaction();
         res.status(200).json({
             data: {
-                "number": orderId
-            }
+                number: orderId,
+            },
         });
     }
     catch (err) {
