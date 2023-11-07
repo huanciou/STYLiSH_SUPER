519fa05290224e5c29f39b1b4112e870131cb4f3
Joazen <johnsons544@gmail.com>
2023-11-07 16:15:01 +0800

    add header

--- server/dist/index.js
+++ server/dist/index.js
@@ -17,55 +17,68 @@ const app = express();
 const port = 3000;
 /////////
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // 允许所有来源
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 /////////
 app.use(cookieParser());
 app.enable("trust proxy");
 const router = Router();
 router.use(function (req, res, next) {
-    next();
+  next();
 });
 app.use(express.json());
 // log
 const log = fs.createWriteStream("./logs/morganBody/morganBody.log", {
-    flags: "a",
+  flags: "a",
 });
 morganBody(app, {
-    noColors: true,
-    stream: log,
+  noColors: true,
+  stream: log,
 });
 app.use("/api", rateLimiter, [
-    productRouter,
-    userRouter,
-    campaignRouter,
-    orderRouter,
-    reportRouter,
+  productRouter,
+  userRouter,
+  campaignRouter,
+  orderRouter,
+  reportRouter,
 ]);
-app.use(branch((req) => req.path.includes("/admin"), [authenticate]), express.static("../client"));
+app.use(
+  branch((req) => req.path.includes("/admin"), [authenticate]),
+  express.static("../client")
+);
 app.use("/uploads", express.static("./uploads"));
 app.use("/assets", express.static("./assets"));
 app.use(errorHandler);
 app.listen(port, () => {
-    console.log(`STYLiSH listening on port ${port}`);
+  console.log(`STYLiSH listening on port ${port}`);
 });
 const outputLogStream = fs.createWriteStream("./logs/console/console.log", {
-    flags: "a",
+  flags: "a",
 });
 if (process.env.SERVER_STATUS === "production") {
-    const originalConsoleLog = console.log;
-    console.log = (...args) => {
-        const message = args.join(" ");
-        outputLogStream.write(message + "\n");
-        originalConsoleLog(...args);
-    };
-    const originalConsoleError = console.error;
-    console.error = (...args) => {
-        const message = args.join(" ");
-        outputLogStream.write(`[ERROR] ${message}\n`);
-        for (const arg of args) {
-            if (arg instanceof Error) {
-                outputLogStream.write(`[ERROR Stack Trace] ${arg.stack}\n`);
-            }
-        }
-        originalConsoleError(...args);
-    };
+  const originalConsoleLog = console.log;
+  console.log = (...args) => {
+    const message = args.join(" ");
+    outputLogStream.write(message + "\n");
+    originalConsoleLog(...args);
+  };
+  const originalConsoleError = console.error;
+  console.error = (...args) => {
+    const message = args.join(" ");
+    outputLogStream.write(`[ERROR] ${message}\n`);
+    for (const arg of args) {
+      if (arg instanceof Error) {
+        outputLogStream.write(`[ERROR Stack Trace] ${arg.stack}\n`);
+      }
+    }
+    originalConsoleError(...args);
+  };
 }
--- server/index.ts
+++ server/index.ts
@@ -21,6 +21,15 @@ const port = 3000;
 /////////
 app.use(cors());
 /////////
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // 允许所有来源
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(cookieParser());
 
